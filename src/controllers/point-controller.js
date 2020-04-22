72eaa35cc66406762e0eb6c690b179a260d0ed63
Semyon <saymon221090@gmail.com>
2020-04-22 15:27:43 +0300

    Делит функцию renderEvent на отдельные методы

--- src/controllers/point-controller.js
+++ src/controllers/point-controller.js
@@ -8,38 +8,40 @@ export default class PontController {
 
     this._eventComponent = null;
     this._eventEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   renderEvent(event) {
     this._eventComponent = new EventComponent(event);
     this._eventEditComponent = new EventEditComponent(event);
 
-    const replaceEventToEdit = () => {
-      this._container.replaceChild(this._eventEditComponent.getElement(), this._eventComponent.getElement());
-    };
-
-    const replaceEditToEvent = () => {
-      this._container.replaceChild(this._eventComponent.getElement(), this._eventEditComponent.getElement());
-    };
-
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-      if (isEscKey) {
-        replaceEditToEvent();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      }
-    };
-
     this._eventComponent.setEditButtonHandler(() => {
-      replaceEventToEdit();
-      document.addEventListener(`keydown`, onEscKeyDown);
+      this._replaceEventToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
     });
 
     this._eventEditComponent.setSubmitHandler(() => {
-      replaceEditToEvent();
+      this._replaceEditToEvent();
     });
 
     render(this._container, this._eventComponent, RenderPosition.BEFOREEND);
+  }
+
+  _replaceEventToEdit() {
+    this._container.replaceChild(this._eventEditComponent.getElement(), this._eventComponent.getElement());
+  }
+
+  _replaceEditToEvent() {
+    this._container.replaceChild(this._eventComponent.getElement(), this._eventEditComponent.getElement());
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToEvent();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
   }
 }
