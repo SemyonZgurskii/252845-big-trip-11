9068a4827da884e5300ef4ad5727635446e15e09
Semyon <saymon221090@gmail.com>
2020-05-05 16:43:22 +0300

    Настраивает отрисовку не привязанных к данным графиков

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,4 +1,6 @@
 import AbstractSmartComponent from "./abstract-smart-component";
+import Chart from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 
 const createStatisticsTemplate = () => {
@@ -21,13 +23,176 @@ const createStatisticsTemplate = () => {
   );
 };
 
+// const moneyCtx = document.querySelector(`.statistic__money`);
+// const transportCtx = document.querySelector(`.statistic__transport`);
+// const timeSpendCtx = document.querySelector(`.statistic__time-spend`);
+
+// // Рассчитаем высоту канваса в зависимости от того, сколько данных в него будет передаваться
+// const BAR_HEIGHT = 55;
+// moneyCtx.height = BAR_HEIGHT * 6;
+// transportCtx.height = BAR_HEIGHT * 4;
+// timeSpendCtx.height = BAR_HEIGHT * 4;
+
+const renderMoneyChart = (moneyCtx) => {
+  return new Chart(moneyCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`FLY`, `STAY`, `DRIVE`, `LOOK`, `RIDE`],
+      datasets: [{
+        data: [400, 300, 200, 160, 100],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `€ ${val}`
+        }
+      },
+      title: {
+        display: true,
+        text: `MONEY`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
+
+const renderTransportChart = (transportCtx) => {
+  return new Chart(transportCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`FLY`, `DRIVE`, `RIDE`],
+      datasets: [{
+        data: [4, 2, 1],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `${val}x`
+        }
+      },
+      title: {
+        display: true,
+        text: `TRANSPORT`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
 
 export default class Statistics extends AbstractSmartComponent {
   constructor() {
     super();
+
+    this._renderChart();
   }
 
   getTemplate() {
     return createStatisticsTemplate();
+  }
+
+  _renderChart() {
+    // debugger;
+    const moneyCtx = this.getElement().querySelector(`.statistics__chart--money`);
+    const transportCtx = this.getElement().querySelector(`.statistics__chart--transport`);
+    const timeSpendCtx = this.getElement().querySelector(`.statistics__chart--time`);
+
+    // Рассчитаем высоту канваса в зависимости от того, сколько данных в него будет передаваться
+    const BAR_HEIGHT = 55;
+    moneyCtx.height = BAR_HEIGHT * 6;
+    transportCtx.height = BAR_HEIGHT * 4;
+    timeSpendCtx.height = BAR_HEIGHT * 4;
+
+    renderMoneyChart(moneyCtx);
+    renderTransportChart(transportCtx);
   }
 }
