bff6892e8df87f449d2b5d7d4ced39cfa461b2e9
Semyon <saymon221090@gmail.com>
2020-05-13 22:47:51 +0300

    Удаляет ненужные данные из const.js

--- src/const.js
+++ src/const.js
@@ -5,21 +5,6 @@ const EVENT_TYPES = {
 
 const CITIES = [`Boston`, `Bolonga`, `Geneva`, `Prague`, `London`, `Wellington`, `Paris`, `San Francisco`];
 
-const MonthNumberToString = {
-  [1]: `JAN`,
-  [2]: `FEB`,
-  [3]: `MAR`,
-  [4]: `APR`,
-  [5]: `MAY`,
-  [6]: `JUN`,
-  [7]: `JUL`,
-  [8]: `AUG`,
-  [9]: `SEP`,
-  [10]: `OCT`,
-  [11]: `NOV`,
-  [12]: `DEC`,
-};
-
 const TypeToIcon = {
   "TAXI": `🚕`,
   "BUS": `🚌`,
@@ -45,7 +30,6 @@ export {
   HIDDEN_CLASS,
   EVENT_TYPES,
   CITIES,
-  MonthNumberToString,
   FilterType,
   TypeToIcon,
 };
